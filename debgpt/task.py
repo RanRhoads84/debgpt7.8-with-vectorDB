8152e5cb140a91e1144615abdb65a7ffe21f3ef8
Mo Zhou <cdluminate@gmail.com>
2024-10-30 00:21:15 -0700

    inplace-git-add-commit: we need two newlines.

--- debgpt/task.py
+++ debgpt/task.py
@@ -79,6 +79,7 @@ def task_git_commit(ag) -> None:
     commit_message = f.session[-1]['content']
     if getattr(ag, 'inplace_git_add_commit', False):
         # is the code automatically modified by debgpt --inplace?
+        commit_message += '\n\n'
         commit_message += '\n'.join(textwrap.wrap(f"\n\nNote, the code changes are made by the command: {' '.join(sys.argv)}.", width=80))
         commit_message += '\n'.join(textwrap.wrap(f"\n\nFrontend used: {ag.frontend}.", width=80))
         if ag.frontend == "openai":
