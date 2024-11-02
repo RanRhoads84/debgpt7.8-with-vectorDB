0d6c5f2a4200f43a0f948270af493cd245770739
Mo Zhou <cdluminate@gmail.com>
2024-11-01 22:40:18 -0700

    policy: improve the class and allow iteration.

--- debgpt/policy.py
+++ debgpt/policy.py
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
+from typing import Union
 import os
 import requests
 from .defaults import console
@@ -48,11 +49,51 @@ class DebianPolicy:
         with open(cache, 'rt') as f:
             self.lines: list[str] = [x.rstrip() for x in f.readlines()]
 
+        # Scan the document and cache the section indexes.
+        self.indexes: list[str] = self.__scan_indexes()
+
+    def __iter__(self):
+        # Return an iterator over the section indexes.
+        self.__cursor :int = 0
+        return self
+
+    def __next__(self) -> str:
+        # Return the next section index.
+        if self.__cursor < len(self.indexes):
+            section = self.indexes[self.__cursor]
+            self.__cursor += 1
+            return self.__getitem__(section)
+        else:
+            raise StopIteration
+
+    def __len__(self) -> int:
+        # Return the number of sections in the document.
+        return len(self.indexes)
+
+    def __scan_indexes(self) -> list[str]:
+        # Scan the document and return a list of all section indexes.
+        ret: list[str] = []
+        for i in range(1, len(self.lines)):
+            cursur = self.lines[i]
+            previous = self.lines[i-1]
+            if any(cursur.startswith(x)
+                   for x in [self.SEP_SECTION,
+                             self.SEP_SUBSECTION, 
+                             self.SEP_SUBSUBSECTION]):
+                index = previous.split(' ')[0]
+                if index.endswith('.'):
+                    ret.append(index.rstrip('.'))
+        return ret
+
     def __str__(self) -> str:
         # Return the entire document as a string.
         return '\n'.join(self.lines)
 
-    def __getitem__(self, index: str) -> str:
+    def __getitem__(self, index: Union[str, int]) -> str:
+        # if the index is an integer, map it to the real section number
+        if isinstance(index, int):
+            section = self.indexes[index]
+            return self.__getitem__(index)
         # Retrieve a specific section, subsection, or subsubsection based on the index.
         sep: str = {
             1: self.SEP_SECTION,
@@ -91,3 +132,17 @@ class DebianDevref(DebianPolicy):
     def __init__(self, cache: str = 'devref.txt') -> None:
         # Initialize the DebianDevref class, inheriting from DebianPolicy.
         super().__init__(cache)
+
+
+if __name__ == '__main__':
+    import numpy as np
+    # Test the DebianPolicy class.
+    p = DebianPolicy()
+    print(len(p))
+    for (sec, text) in zip(p.indexes, p):
+        print('section', sec, 'length', len(text.encode()), 'bytes')
+
+
+    # Test the DebianDevref class.
+    d = DebianDevref()
+    print(len(d['1.1']))
